006e2c5d4d142cd5cc1ec0dacc3cd24ec96c97a9
Sagar Saluja <sagar.saluja@onsolve.com>
2023-01-22 17:55:56 +0530

    setup

---
+++ Explosion.js
@@ -0,0 +1,13 @@
+/** @type {HTMLCanvasElement} */
+//this above line suggests built-in functions on typing context.
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 800;
+
+const animate = () => {
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  requestAnimationFrame(animate);
+};
+animate();
